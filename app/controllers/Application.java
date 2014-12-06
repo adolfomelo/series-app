b08f017c3f3e9c3520c712a362b81294310e178d
Gustavo Alves <gugamalves@yahoo.com.br>
2014-12-06 19:31:52 -0300

    [VIEW] Started index view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,24 @@
 package controllers;
 
+import models.Series;
+import models.dao.GenericDAO;
+import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
+import java.util.List;
+
 public class Application extends Controller {
+    static final GenericDAO dao = new GenericDAO();
 
     public static Result index() {
-        return ok(index.render());
+        return redirect(routes.Application.series());
     }
 
+    @Transactional
+    public static Result series() {
+        List<Series> seriesList = dao.findAllByClass(Series.class);
+        return ok(views.html.index.render(seriesList));
+    }
 }
